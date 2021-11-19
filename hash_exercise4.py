6bb273428ae2d2b91d47c5e597237e9631fd1934
Christos Gogos <chgogos@gmail.com>
2021-11-19 02:31:30 +0200

    disjoint sets

---
+++ hash_exercise4.py
@@ -0,0 +1,14 @@
+input = [17, 13, 21, 19, 5]
+print(input)
+
+# διατήρηση της θέσης στην οποία βρίσκεται η κάθε τιμή του input
+d = {}
+for i,e in enumerate(input):
+    d[e] = i
+
+rank = 1    
+for e in sorted(d.keys()):
+    input[d[e]] = rank
+    rank += 1
+    
+print(input)
