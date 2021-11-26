846508ee9f16b4e3943b5ac6b86ed77be5eb908a
Christos Gogos <chgogos@gmail.com>
2021-11-26 03:22:22 +0200

    sorting

---
+++ quicksort.py
@@ -0,0 +1,9 @@
+def quick_sort(array):
+    if len(array) < 2:
+        return array
+    pivot = array[0]
+    less = [i for i in array[1:] if i <= pivot]
+    greater = [i for i in array[1:] if i > pivot]
+    return quick_sort(less) + [pivot] + quick_sort(greater)
+    
+print(quick_sort([3,5,2,1,4]))
