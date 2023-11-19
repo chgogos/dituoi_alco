66429af4d219fefa3baabfe3f315c38e5f659309
Christos Gogos <chgogos@gmail.com>
2023-11-20 00:05:18 +0200

    minor

--- hash_exercise4.py
+++ hash_exercise4.py
@@ -3,15 +3,14 @@ print(input)
 
 # διατήρηση της θέσης στην οποία βρίσκεται η κάθε τιμή του input
 d = {}
-for i,e in enumerate(input):
+for i, e in enumerate(input):
     d[e] = i
 
-rank = 1    
-for e in sorted(d.keys()):
-    input[d[e]] = rank
-    rank += 1
-    
-print(input)
+output = [0] * len(input)
+for i, e in enumerate(sorted(d.keys())):
+    output[d[e]] = i + 1
+
+print(output)
 
 # [17, 13, 21, 19, 5]
 # [3, 2, 5, 4, 1]
